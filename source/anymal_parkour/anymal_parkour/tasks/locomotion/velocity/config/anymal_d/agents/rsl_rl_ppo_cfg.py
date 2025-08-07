f8b6e09b0d33c826e6fb14241f07d697fdd2ec2b
benjy <benjymccartney@putlook.com>
2025-08-07 10:56:28 +0100

    Changed flat network to match rough network

--- source/anymal_parkour/anymal_parkour/tasks/locomotion/velocity/config/anymal_d/agents/rsl_rl_ppo_cfg.py
+++ source/anymal_parkour/anymal_parkour/tasks/locomotion/velocity/config/anymal_d/agents/rsl_rl_ppo_cfg.py
@@ -38,5 +38,5 @@ class AnymalDFlatPPORunnerCfg(AnymalDRoughPPORunnerCfg):
 
         self.max_iterations = 1000
         self.experiment_name = "anymal_d_flat"
-        self.policy.actor_hidden_dims = [128, 128, 128]
-        self.policy.critic_hidden_dims = [128, 128, 128]
+        self.policy.actor_hidden_dims = [512, 256, 128]
+        self.policy.critic_hidden_dims = [512, 256, 128]
